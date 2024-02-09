af764fb7c2a694b455eb8af2564793d810e0ada3
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 16:29:39 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,38 +1,42 @@
-import java.util.Scanner;
+import java.util.*;
 // package edu.iastate.cs228.hw1;
+// @author Michael Jones
 
-public static void main(String[] args){
+public class Main{
 
-  Scanner s = new Scanner(System.in);
-  ISPBuisness buisness = new ISPBuisness();
-  boolean intialized = false;
-  String str;
-  int i;
+  public static void main(String[] args){
 
-  while(initialized == false){
-    System.out.println("How to populate grid (type 1 or 2): 1: from file text. 2: randomly with seed");
-    i = s.nextInt();
-    if(i == 1) { // initializes the grid based off text from a file
-      System.out.println("Please enter file text.");
-      s.nextLine();
-      str = s.nextLine();
-      buisness.generateGrid(str);
-      initialized = true;
-    } else if(i == 2) { // initializes the grid randomly
-      s.nextLine();
-      while (initialized == false) {
-        System.out.println("Provide the desired number of rows, columns, and the seed integer separated by spaces:");
+    Scanner s = new Scanner(System.in);
+    ISPBuisness buisness = new ISPBuisness();
+    boolean initialized = false;
+    String str;
+    int i;
+
+    while(initialized == false){
+      System.out.println("How to populate grid (type 1 or 2): 1: from file text. 2: randomly with seed");
+      i = s.nextInt();
+      if(i == 1) { // initializes the grid based off text from a file
+        System.out.println("Please enter file text.");
+        s.nextLine();
         str = s.nextLine();
-        String[] arr = str.split(" ");
-        if(arr.length == 3) {
-          buisness.generateGrid(arr[0], arr[1], arr[2]);
-          initialized = true;
-        } else {
-          System.out.println("Invalid input, please try again");
+        buisness.generateGrid(str);
+        initialized = true;
+      } else if(i == 2) { // initializes the grid randomly
+        s.nextLine();
+        while (initialized == false) {
+          System.out.println("Provide the desired number of rows, columns, and the seed integer separated by spaces:");
+          str = s.nextLine();
+          String[] arr = str.split(" ");
+          if(arr.length == 3) {
+            buisness.generateGrid(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
+            initialized = true;
+          } else {
+            System.out.println("Invalid input, please try again");
+          }
         }
+      } else {
+        System.out.println("Invalid input, please try again.");
       }
-    } else {
-      System.out.println("Invalid input, please try again.");
     }
   }
 }
