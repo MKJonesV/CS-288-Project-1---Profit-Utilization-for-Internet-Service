bde8c7127bb98fd6b3bf6b7740841603fa648830
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 12:09:31 -0600

    Update ISPBusiness.java

--- ISPBusiness.java
+++ ISPBusiness.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+package edu.iastate.cs228.hw1;
 public class ISPBuisness {
+}
+
+public static void main(String[] args){
+  Scanner s = new Scanner(System.in);
+  boolean intialized = false;
+  while(initialized == false);
+    System.out.println("How to populate grid (type 1 or 2): 1: from a file. 2: randomly with seed");
+    int i = s.nextInt();
+    if(i == 1){
+      initialized = true;
+    }else if(i == 2){
+      initialized = true;
+    }else{
+      System.out.println("Invalid input, please try again.");
+    }
+  String grid = 
 }
