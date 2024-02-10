883fc337c6997ee4fab292155ddf3c8f8c352509
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 19:05:44 -0600

    Update Reseller.java

--- Reseller.java
+++ Reseller.java
@@ -1,5 +1,13 @@
-package edu.iastate.cs228.hw1;
+//package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Reseller extends TownCell {
+
+  public Reseller(Town p, int r, int c) {
+    super(p, r, c);
+  }
+  
+  public String toString() {
+    return "R";
+  }
 }
