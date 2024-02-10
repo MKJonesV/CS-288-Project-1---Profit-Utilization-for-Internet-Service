87f7482c3c9d7b3238b145bb5bf16c1e130f0bf6
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 19:05:19 -0600

    Update Outage.java

--- Outage.java
+++ Outage.java
@@ -1,6 +1,14 @@
-package edu.iastate.cs228.hw1;
+//package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Outage extends TownCell {
+
+  public Outage(Town p, int r, int c) {
+    super(p, r, c);
+  }
   
+  public String toString() {
+    return "O";
+  }
+
 }
