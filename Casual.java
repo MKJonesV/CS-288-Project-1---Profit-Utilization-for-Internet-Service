991ba2af6b44a3c5539816fb1bff65720509c178
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-10 15:31:57 -0600

    Update Casual.java

--- Casual.java
+++ Casual.java
@@ -1,8 +1,11 @@
-//package edu.iastate.cs228.hw1;
+package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Casual extends TownCell {
 
+  State s = State.CASUAL;
+  TownCell t;
+  
   public Casual(Town p, int r, int c) {
     super(p, r, c);
   }
@@ -10,4 +13,21 @@ public class Casual extends TownCell {
   public String toString() {
     return "C";
   }
+
+  public State who(){
+    return s;
+  }
+
+  @Override
+  public TownCell next(Town tNew){
+    census(nCensus);
+    if(nCensus[RESELLER] > 0){
+      t = new Outage(tNew, row, col);
+    }else if(nCensus[STREAMER] > 0){
+      t = new Streamer(tNew, row, col);
+    }else{
+      t = new Casual(tNew, row, col);
+    }
+    return finalCases(this, t, tNew);
+  }  
 }
