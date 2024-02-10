3c2fc44bd82054968d35616e23609b35dc6eb5dd
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-10 15:30:31 -0600

    Update Streamer.java

--- Streamer.java
+++ Streamer.java
@@ -1,8 +1,11 @@
-//package edu.iastate.cs228.hw1;
+package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Streamer extends TownCell {
 
+  State s = State.STREAMER;
+  TownCell t;
+  
   public Streamer(Town p, int r, int c) {
     super(p, r, c);
   }
@@ -10,4 +13,21 @@ public class Streamer extends TownCell {
   public String toString() {
     return "S";
   }
+
+  public State who(){
+    return s;
+  }
+
+  @Override
+  public TownCell next(Town tNew){
+    census(nCensus);
+    if(nCensus[RESELLER] > 0){
+      t = new Outage(tNew, row, col);
+    }else if(nCensus[OUTAGE] > 0){
+      t = new Empty(tNew, row, col);
+    }else{
+      t = new Streamer(tNew, row, col);
+    }
+    return finalCases(this, t, tNew);
+  } 
 }
