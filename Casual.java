d79341f646c7346533cf73c47119df1195f3bfa8
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 19:04:22 -0600

    Update Casual.java

--- Casual.java
+++ Casual.java
@@ -1,5 +1,13 @@
-package edu.iastate.cs228.hw1;
+//package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Casual extends TownCell {
+
+  public Casual(Town p, int r, int c) {
+    super(p, r, c);
+  }
+  
+  public String toString() {
+    return "C";
+  }
 }
