f1bb031eaf1f50fbe218e70309976e5180871eb1
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 19:06:00 -0600

    Update Streamer.java

--- Streamer.java
+++ Streamer.java
@@ -1,5 +1,13 @@
-package edu.iastate.cs228.hw1;
+//package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Streamer extends TownCell {
+
+  public Streamer(Town p, int r, int c) {
+    super(p, r, c);
+  }
+  
+  public String toString() {
+    return "S";
+  }
 }
