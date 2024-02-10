96c78f403592fd2e4dff2ff590eb67ab0bc152ab
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-09 19:04:52 -0600

    Update Empty.java

--- Empty.java
+++ Empty.java
@@ -1,6 +1,13 @@
-package edu.iastate.cs228.hw1;
+//package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Empty extends TownCell {
+
+  public Empty(Town p, int r, int c) {
+    super(p, r, c);
+  }
   
+  public String toString() {
+    return "E";
+  }
 }
