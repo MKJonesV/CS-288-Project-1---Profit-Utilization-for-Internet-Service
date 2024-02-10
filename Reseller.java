b6a7f451b537154580a30aadf5e695b25d8b56c5
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-10 15:31:00 -0600

    Update Reseller.java

--- Reseller.java
+++ Reseller.java
@@ -1,8 +1,11 @@
-//package edu.iastate.cs228.hw1;
+package edu.iastate.cs228.hw1;
 // @author Michael Jones
 
 public class Reseller extends TownCell {
 
+  State s = State.RESELLER;
+  TownCell t;
+  
   public Reseller(Town p, int r, int c) {
     super(p, r, c);
   }
@@ -10,4 +13,19 @@ public class Reseller extends TownCell {
   public String toString() {
     return "R";
   }
+
+  public State who(){
+    return s;
+  }
+
+  @Override
+  public TownCell next(Town tNew){
+    census(nCensus);
+    if(nCensus[CASUAL] <= 3 || nCensus[EMPTY] >= 3){
+      t = new Empty(tNew, row, col);
+    }else{
+      t = new Reseller(tNew, row, col);
+    }
+    return finalCases(this, t, tNew);
+  } 
 }
