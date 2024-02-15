9c1f65b41328d80a0e55980a25ba707738d92b54
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-15 16:11:21 -0600

    Create OutageTest.java

---
+++ OutageTest.java
@@ -0,0 +1,17 @@
+package edu.iastate.cs228.hw1.test;
+  //@author Michael Jones
+import static org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OutageTest {
+
+  @Test 
+  void checkMethods(){
+    Town t = new Town(3, 3);
+    Outage o = new Outage(t, 0, 0);
+    State expected = State.OUTAGE;
+    State actual = o.who();
+
+    assertEquals(expected, actual);
+  }
+}
