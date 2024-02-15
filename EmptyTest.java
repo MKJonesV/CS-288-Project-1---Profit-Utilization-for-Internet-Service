cacc5fc2594ea8f7cb22be856566a55faf8c3e0b
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-15 16:19:18 -0600

    Update EmptyTest.java

--- EmptyTest.java
+++ EmptyTest.java
@@ -2,10 +2,11 @@ package edu.iastate.cs228.hw1.test;
   //@author Michael Jones
 import static org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import edu.iastate.cs228.hw1.*;
 
 public class EmptyTest {
 
-  @Test 
+  //@Test 
   void checkMethods(){
     Town t = new Town(3, 3);
     Empty e = new Empty(t, 0, 0);
