d5b8eda424da4903f57734d4b2e9b511589dda42
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-15 16:18:23 -0600

    Update ResellerTest.java

--- ResellerTest.java
+++ ResellerTest.java
@@ -2,10 +2,11 @@ package edu.iastate.cs228.hw1.test;
   //@author Michael Jones
 import static org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import edu.iastate.cs228.hw1.*;
 
 public class ResellerTest {
 
-  @Test 
+  //@Test 
   void checkMethods(){
     Town t = new Town(3, 3);
     Reseller r = new Reseller(t, 0, 0);
