1cf9c2b77398f749810ae340828d2f41f0776870
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-02-15 16:12:09 -0600

    Create StreamerTest.java

---
+++ StreamerTest.java
@@ -0,0 +1,17 @@
+package edu.iastate.cs228.hw1.test;
+  //@author Michael Jones
+import static org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StreamerTest {
+
+  @Test 
+  void checkMethods(){
+    Town t = new Town(3, 3);
+    Streamer s = new Streamer(t, 0, 0);
+    State expected = State.STREAMER;
+    State actual = s.who();
+
+    assertEquals(expected, actual);
+  }
+}
